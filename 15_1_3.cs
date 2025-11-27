2b334746c4be9b55c3cfa3cd6759dbcc48928e83
Vlados <bykovvlados@vk.com>
2025-11-27 06:43:30 +0400

    Create 15_1_3.cs

---
+++ 15_1_3.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("input.txt");
+
+        string[] numbersStr = lines[0].Split(' ');
+        int[] array = new int[numbersStr.Length];
+        for (int i = 0; i < numbersStr.Length; i++)
+        {
+            array[i] = int.Parse(numbersStr[i]);
+        }
+
+        string[] rangeStr = lines[1].Split(' ');
+        int a = int.Parse(rangeStr[0]);
+        int b = int.Parse(rangeStr[1]);
+
+        List<int> result = new List<int>();
+
+        foreach (int num in array)
+        {
+            if (num >= a && num <= b)
+            {
+                result.Add(num); 
+            }
+        }
+
+        result.Sort();
+
+        using (StreamWriter writer = new StreamWriter("output.txt"))
+        {
+            if (result.Count == 0)
+            {
+                writer.WriteLine("Нет чисел в заданном диапазоне.");
+                Console.WriteLine("Нет чисел в заданном диапазоне.");
+            }
+            else
+            {
+                foreach (int num in result)
+                {
+                    writer.WriteLine(num);
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
