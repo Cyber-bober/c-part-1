685b0248de368e2608201aa1161adb5a7ca80573
Vlados <bykovvlados@vk.com>
2025-12-11 13:31:06 +0400

    Update 15_1_3.cs

--- 15_1_3.cs
+++ 15_1_3.cs
@@ -14,11 +14,12 @@ class Program
     static void Main()
     {
         char[] delimiters = { ' ', '\t', '\n', '\r', ',', ';', ':', '.', '!', '?', '(', ')', '[', ']', '{', '}' };
-        var numbers = File.ReadAllText("E:/study/ВУЗ/3 семестр/структура данных и алгоритмы/code3smtr/15_1_3/ConsoleApp1/input.txt")
-                           .Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
-                           .Where(s => int.TryParse(s, out _))
-                           .Select(s => int.Parse(s))
-                           .ToList();
+        string text = File.ReadAllText("E:/study/ВУЗ/3 семестр/структура данных и алгоритмы/code3smtr/15_1_3/ConsoleApp1/input.txt")
+        string[] tokens = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        var allNumbers = (from token in tokens
+                          where int.TryParse(token, out _) 
+                          select int.Parse(token)).ToList();
 
         if (numbers.Count < 2)
         {
@@ -34,11 +35,13 @@ class Program
             a = b;
             b = x;
         }
-
-        var result = numbers.Take(numbers.Count - 2)
-                            .Where(n => n >= a && n <= b)
-                            .OrderBy(n => n)
-                            .ToList();
+        var sequence = new int[allNumbersCount -2];
+        for (int i = 0; i < sequence.Lenth; i++)
+            sequence[i] = AllNumbers[i]
+        var result = (for n in sequence
+                      where n >= a && n <= b
+                      orderby n
+                      select n).ToList();
 
         if (result.Count == 0)
             File.WriteAllText("E:/study/ВУЗ/3 семестр/структура данных и алгоритмы/code3smtr/15_1_3/ConsoleApp1/output.txt", "Нет чисел в заданном диапазоне.");
