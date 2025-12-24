e5d589ab179d7f5f7c6bf3a89e067507ef4b6af6
Vlados <bykovvlados@vk.com>
2025-12-24 08:18:49 +0400

    Update 15_1_3.cs

--- 15_1_3.cs
+++ 15_1_3.cs
@@ -4,7 +4,7 @@
 данных – файловый. 
 
 Вывести на экран все числа, попадающий в отрезок [a, b], отсортировав их в порядке 
-возрастания. */
+возрастания. -10 -9 -8 -7 -6 -5 -4 -3 ^ -2 -1 0&% 0 1 2 3 ! 1 2 3 №;*(:№% 4 5 6 7 8 9 10 -5 6 */
 using System;
 using System.IO;
 using System.Linq;
@@ -14,38 +14,47 @@ class Program
     static void Main()
     {
         char[] delimiters = { ' ', '\t', '\n', '\r', ',', ';', ':', '.', '!', '?', '(', ')', '[', ']', '{', '}' };
-        string text = File.ReadAllText("E:/study/ВУЗ/3 семестр/структура данных и алгоритмы/code3smtr/15_1_3/ConsoleApp1/input.txt")
+        string text = File.ReadAllText("E:/study/ВУЗ/3 семестр/структура данных и алгоритмы/code3smtr/15_1_3/ConsoleApp1/input.txt");
         string[] tokens = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
 
-        var allNumbers = (from token in tokens
-                          where int.TryParse(token, out _) 
-                          select int.Parse(token)).ToList();
+        var allNumbersQuery = from token in tokens
+                              where int.TryParse(token, out int number)
+                              select number;
 
-        if (numbers.Count < 2)
+        int[] allNumbersArray = allNumbersQuery.ToArray();
+
+        if (allNumbersArray.Length < 2)
         {
             File.WriteAllText("E:/study/ВУЗ/3 семестр/структура данных и алгоритмы/code3smtr/15_1_3/ConsoleApp1/output.txt", "Ошибка: недостаточно чисел для определения диапазона [a, b].");
             return;
         }
 
-        int a = numbers[numbers.Count - 2];
-        int b = numbers[numbers.Count - 1];
+        int a = allNumbersArray[allNumbersArray.Length - 2];
+        int b = allNumbersArray[allNumbersArray.Length - 1];
         if (a > b)
         {
-            int x = a;
+            int temp = a;
             a = b;
-            b = x;
+            b = temp;
         }
-        var sequence = new int[allNumbersCount - 2];
-        for (int i = 0; i < sequence.Length; i++)
-            sequence[i] = AllNumbers[i]
-        var result = (for n in sequence
-                      where n >= a && n <= b
-                      orderby n
-                      select n).ToList();
 
-        if (result.Count == 0)
+        var sequenceQuery = from i in Enumerable.Range(0, allNumbersArray.Length - 2)
+                            select allNumbersArray[i];
+
+        var resultQuery = from n in sequenceQuery
+                          where n >= a && n <= b
+                          orderby n
+                          select n.ToString();
+
+        string[] resultArray = resultQuery.ToArray();
+
+        if (resultArray.Length == 0)
+        {
             File.WriteAllText("E:/study/ВУЗ/3 семестр/структура данных и алгоритмы/code3smtr/15_1_3/ConsoleApp1/output.txt", "Нет чисел в заданном диапазоне.");
+        }
         else
-            File.WriteAllLines("E:/study/ВУЗ/3 семестр/структура данных и алгоритмы/code3smtr/15_1_3/ConsoleApp1/output.txt", result.Select(n => n.ToString()));
+        {
+            File.WriteAllLines("E:/study/ВУЗ/3 семестр/структура данных и алгоритмы/code3smtr/15_1_3/ConsoleApp1/output.txt", resultArray);
+        }
     }
 }
